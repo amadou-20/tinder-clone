3b9343fc524799be9b81467287c257acac944dd3
amadou-20 <ahmo300@outlook.com>
2020-11-06 00:32:48 +0100

    the tinder-clone project

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+
+
+function Header() {
+
+  return (
+    <div className="App">
+        
+        <personIcon />
+      <h1> He is a person</h1>
+    </div>
+  );
+}
+
+export default Header;
